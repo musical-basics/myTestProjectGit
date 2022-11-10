aabd9b7dc9610f3f2c2541c606932163724efc84
Lionel Yu <lionel@musicalbasics.com>
2022-11-10 12:48:20 -0800

    Initial Commit

---
+++ myTestProjectGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myTestProjectGit
+//
+//  Created by Lionel Yu on 11/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
